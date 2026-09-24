7e8bf3d52ba27927bb3dfb787062ee2e98b2ad28
mahmudstat <almahmud.sbi@gmail.com>
2026-09-24 13:14:05 +0600

    add modulo

--- courses/clang/code/operators.c
+++ courses/clang/code/operators.c
@@ -1,129 +1,60 @@
 #include <stdio.h>
 
 int main(){
-  //Int
-  int area = 24;
-  float height = 2.3;
-  char jar = 'A';
+  // Basic operations
+  int x = 35;
+  int y = 20;
+  int sum = x + y;
+  int diff = x - y;
+  int prod = x * y;
+  float div = (float) x / y;
 
-  float volume = area * height;
-  printf("The volume of the jar %c is %f\n", jar, volume);
+  printf("The sum is %d\n", sum);
+  printf("The division is %f\n", div);
 
-  // ASCII for char
-  char a = 65, b = 67;
-  printf("The ASCII char is %c\n", a);
-
-  //  Storing more than a character
-  char c = 'Language';
-  printf("The text is %c\n", c); //just e
-
-  //Need string to store multiple characters (%s)
-  char fullchar[] = "Language";
-  printf("Now we should ge the whole %s\n", fullchar);
-
-
-  // Double allows more precision than float (7 vs 15 digits)
-
-  float len = 19.101255564;
-  float wid = 2.672;
-  float area1 = len * wid;
-  double area2 = len * wid;
-  printf("The area in float is %f and in double is %lf\n", area1, area2); // Still same output, more on this later
-
-  // Scientific numbers
-  float sciN = 10e2;
-  double sciN2 = 34E3;
-  printf("%f, %lf\n", sciN, sciN2);
-
-  // Set Decimal precision
-
-  float num1 = 4.5;
-  double num2 = 5.6;
-  printf("%f \n", num1);
-  printf("%lf\n", num2);
-
-  //Specify number of digits after decimal
-  printf("The whole number is %f\n", num2);
-  printf("The number up to first digit after decimal is %.1lf\n", num2);
-
-  // Operator Size
-  printf("The size of the area int is %zu\n", sizeof(area));
-  printf("The size of the jar char is %zu\n", sizeof(jar));
-  printf("The size of the float is %zu\n", sizeof(num1));
-  printf("The size of the double is %zu\n", sizeof(num2));
-
-  // Example of different data type
-  int count = 20;
-  float totweight = 65.23;
-  float avgweight = totweight / count;
-  printf("The avaerage weight is %f\n", avgweight);
-
-  //All data types
-
-  int intg = 200;
-  double db = 4.45;
-
-  short int tiny = 5; // range -32,768 to 32,767
-  unsigned int numb = 30;
-
-  long int big = 3462746;
-  long long int large = 4348758542365;
-  unsigned long long int giant = 3462893462346823764;
-  long double euler = 2.7182818285;
-
-  printf("Ordinary integer %d\n", intg);
-  printf("Double %lf\n", db);
-  printf("Small value %hd\n", tiny);
-  printf("A count %u, usually used for counting\n", numb);
-  printf("A Big boss %ld\n", big);
-  printf("A larger one %lld\n", large);
-  printf("A gigantic one now %llu\n", giant);
-  printf("Finally, a precise one %Lf\n", euler);
-
-  // Check their sizes
-  printf("The sizes are \n");
-  printf("%zu, %zu, %zu, %zu, %zu, %zu, %zu, %zu\n", sizeof(intg), sizeof(db), sizeof(tiny), sizeof(numb), sizeof(big), sizeof(large), sizeof(giant), sizeof(euler));
-
-  // Data Tape Conversion
-
-  // The problem
-
-  int x = 20;
-  int y = 3;
-  int div = x / y;
-  printf("%d\n", div); // 6 but it should be 6.66...
-
-  // Implicit conversion
-  // automatically by the compiler
-
-  float fl = 9;
-  printf("%f\n", fl); //converted to 9.00000
-  int z = 8.80;
-  printf("%d \n", z); // converted to 8 from 8.80
-
-  // Even this is a problem
-  float dv = x / y;
-  printf("%f\n", dv); //yet gives 6.00000
-
-  // Explicit conversion
-
-  float correctdv = (float) x / y;
-  printf("%f\n", correctdv); // Now 6.666667
-  printf("%.2f\n", correctdv); // with specific precision
+    // Modulus
+  printf("The modulus is %d\n", x % y);
+  // Increment
+  printf("The incremented value is %d\n", ++x);
+  // Decrement
+  printf("The decremented value is %d\n", x--);
+  //or write, actually ++x and x++ server different purposes.
+  printf("The decremented value is %d\n", --x);
 
   // Example
+  // Counting Students
 
-  int numSick = 20;
-  int totPeople = 200;
-  float ratio1 = numSick / totPeople;
-  float ratio2 = (float) numSick / totPeople;
-  printf("%f, %f\n", ratio1, ratio2);
+  int numStd = 20;
+  // 1 new
+  numStd++;
+  //another comes
+  numStd++;
+  printf("Now there are %d students.\n", numStd);
+  //one goes
+  numStd--;
+  printf("Now there are %d students.\n", numStd);
 
-  // Constants, unchangeable
-  const birth_year = 2001;
-  const birth_month = 12;
-  const DAY = 7; //better to write in uppercase
+  //Assignment operators
+  // = is already familiar to us
 
-  printf("Born in %d.\n", birth_year);
+  //+=
+  int p = 4;
+  p = p + 4;
+  printf("New value is %d \n", p);
+  // can also b written as
+  p += 3;
+  p -= 3;
+  printf("New value is back to %d \n", p);
+
+  p *= 3;
+  printf("New value is after multiplications is %d \n", p);
+
+  //Division
+  p /= 4;
+  printf("New value is after division is %d \n", p);
+
+  //Modulus
+  p %= 5;
+  printf("Modulus is %d \n", p);
   return 0;
 }
